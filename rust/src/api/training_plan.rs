6617882be89627b339501f6779f86ccfb57dd3da
Pavel Oborin <pavel.oborin@gmail.com>
2024-10-09 12:27:51 +1100

    remove unnessesary code

--- rust/src/api/training_plan.rs
+++ rust/src/api/training_plan.rs
@@ -1,8 +1,5 @@
-use crate::BACKTRACE;
 use flutter_rust_bridge::frb;
-use log::info;
 use serde::{Deserialize, Serialize};
-use std::panic;
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct TrainingPlan {
@@ -12,26 +9,8 @@ pub struct TrainingPlan {
 impl TrainingPlan {
     #[frb(sync)]
     pub fn test_deserialize(content: String) -> Result<Self, String> {
-        info!("<!> deserializing training plan");
-
-        let result = panic::catch_unwind(|| {
-            serde_json::from_str(&content)
-        });
-
-        match result {
-            Ok(Ok(plan)) => Ok(plan),
-            Ok(Err(e)) => {
-                info!("Deserialization error: {}", e.to_string());
-                Err(e.to_string())
-            }
-            Err(e) => {
-                let b = BACKTRACE.take().unwrap();
-                info!("at panic:\n{}", b);
-                let err = format!("A panic occurred during deserialization: {e:?}");
-                info!("{}", err);
-                Err(err)
-            }
-        }
+        serde_json::from_str(&content).map_err(|e| e.to_string())
+        
     }
 }
 
